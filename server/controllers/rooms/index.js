4fd594fd9a10f0f9df1781fca9deea268ba61ee0
aereeeee <dofl5576@gmail.com>
2019-10-16 23:19:05 +0900

    [FEATURE] 숙소 검색 API 추가 구현 (#7)
    why
    request의 query에 따라 동적으로 sequelize 쿼리문이 실행될 필요가 있다.
    what
    searchRoomCheck라는 미들웨어를 만들어서 실행해야할 쿼리문을 request에 담아 넘겼다.

--- server/controllers/rooms/index.js
+++ server/controllers/rooms/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const { searchRoomCheck } = require('../../middlewares/searchRoomCheck');
 
 const router = Router();
 const ctrl = require('./rooms.ctrl');
 
 router.get('/', ctrl.getAllRooms);
-router.get('/search', ctrl.getFilteredRooms);
+router.get('/search', searchRoomCheck, ctrl.getFilteredRooms);
 
 module.exports = router;
--- server/controllers/rooms/rooms.ctrl.js
+++ server/controllers/rooms/rooms.ctrl.js
@@ -3,7 +3,6 @@ const paginate = require('../../utils/pagenation');
 
 const getAllRooms = async (req, res, next) => {
   const pageOption = paginate(req.query.page, process.env.PAGE_LIMIT);
-
   try {
     const rooms = await models.Room.findAll(pageOption);
     res.status(200).send(rooms);
@@ -12,6 +11,24 @@ const getAllRooms = async (req, res, next) => {
   }
 };
 
-const getFilteredRooms = async (req, res, next) => {};
+const getFilteredRooms = async (req, res, next) => {
+  const pageOption = paginate(req.query.page, process.env.PAGE_LIMIT);
+
+  try {
+    const filteredRooms = await models.Room.findAll({
+      ...pageOption,
+      where: req.roomQuery,
+      include: [
+        {
+          model: models.Reservation,
+          where: req.reservationQuery,
+        },
+      ],
+    });
+    res.status(200).send(filteredRooms);
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = { getAllRooms, getFilteredRooms };
