3ed6c96bc824b423b6c99d383535ed1d0364e221
aereeeee <dofl5576@gmail.com>
2019-10-16 21:37:40 +0900

    [FEATURE] accounts 관련 API 작성 (#7)
    why
    유저 회원가입, 로그인, 로그아웃을 구현하기 위해
    what
    user 모델을 이용하여 POST메소드로 회원가입 정보를 처리하는 join 라우터
    POST메소드로 유저로그인을 처리하고 토큰을 발행하는 login 라우터
    GET메소드로 유저 인증을 처리하는 islogin 라우터를 작성하였다

--- server/app.js
+++ server/app.js
@@ -32,6 +32,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', require('./controllers'));
+
 app.use((req, res, next) => {
   next(createError(404));
 });
---
+++ server/controllers/accounts/accounts.ctrl.js
@@ -0,0 +1,45 @@
+const models = require('../../models');
+const auth = require('../../middlewares/auth');
+const passwordHash = require('../../utils/passwordHash');
+
+const postJoin = async (req, res, next) => {
+  try {
+    const user = await models.User.create(req.body);
+    const token = auth.signToken(user);
+    res.cookie('access-token', token);
+    res.status(200).end();
+  } catch (err) {
+    next(err);
+  }
+};
+
+const postLogin = async (req, res, next) => {
+  const { userId, pw } = req.body;
+  try {
+    const user = await models.User.findOne({
+      where: { userId, pw: passwordHash(pw) },
+    });
+    if (!user) {
+      res.status(204).end();
+    } else {
+      const token = auth.signToken(user);
+      res.cookie('access-token', token);
+      res.status(200).end();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getisLogin = async (req, res) => {
+  res.status(200).send(req.user);
+};
+
+const getLogout = (req, res) => {};
+
+module.exports = {
+  postJoin,
+  postLogin,
+  getisLogin,
+  getLogout,
+};
