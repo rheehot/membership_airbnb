25ed161927f36b2746c2da20990d508a80de85c3
aereeeee <dofl5576@gmail.com>
2019-10-16 13:47:28 +0900

    [FIX] 서비스 실행시 db connect 이후 바로 sync하도록 수정
    why
    sequelize를 이용하여 작성한 모델과 디비를 최초 실행 시  동기화하기 위해서

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ db.sequelize
   })
   .then(() => {
     console.log('DB Sync complete.');
+    return db.sequelize.sync();
   })
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
