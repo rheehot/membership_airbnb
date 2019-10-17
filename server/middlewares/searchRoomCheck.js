40383257ef8b43cf3feab5ea2324efc75a1ccdb4
aereeeee <dofl5576@gmail.com>
2019-10-17 16:11:33 +0900

    [FIX] 예약시 Reservation 테이블 쿼리 추가 #7
    why
    사용자가 원하는 예약 날짜가 이미 예약된 날짜와 겹치는 상황 발생
    what
    sequeslize operator not,gte,lte 를 사용하여 날짜가 아예 겹치는 데이터를 제외하도록  처리하였다.

--- server/middlewares/searchRoomCheck.js
+++ server/middlewares/searchRoomCheck.js
@@ -1,11 +1,9 @@
 const Sequelize = require('sequelize');
+const dayjs = require('dayjs');
 
 const op = Sequelize.Op;
 
 const searchRoomCheck = (req, res, next) => {
-  const roomQuery = {};
-  const reservationQuery = {};
-
   const {
     min_price,
     max_price,
@@ -17,38 +15,42 @@ const searchRoomCheck = (req, res, next) => {
     num_bathroom,
   } = req.query;
 
+  const roomQuery = {};
+  const reservationQuery = {};
+
   if (min_price || max_price) {
-    let min;
-    let max;
-    if (!min_price) {
-      min = 0;
-    } else if (!max_price) {
-      max = Infinity;
-    } else {
-      roomQuery.price = { [op.between]: [min, max] };
-    }
+    const min = min_price || 0;
+    const max = max_price || Infinity;
+
+    roomQuery.price = { [op.between]: [min, max] };
   }
 
-  if (check_in && check_out) {
-    reservationQuery.checkIn = {
-      [op.notBetween]: [check_in, check_out],
-    };
-    reservationQuery.checkOut = {
-      [op.notBetween]: [check_in, check_out],
-    };
+  if (check_in || check_out) {
+    const checkIn = check_in || dayjs(check_out).subtract(1, 'day').format('YYYY-MM-DD');
+    const checkOut = check_out || dayjs(check_in).add(1, 'day').format('YYYY-MM-DD');
+
+    const dateQuery = { [op.notBetween]: [checkIn, checkOut] };
+    reservationQuery.check_in = dateQuery;
+    reservationQuery.check_out = dateQuery;
+
+    const { not } = op;
+    reservationQuery[not] = [
+      { check_in: { [op.lte]: new Date(checkIn) } },
+      { check_out: { [op.gte]: new Date(checkOut) } },
+    ];
   }
 
   if (num_guest) {
     roomQuery.num_guest = { [op.gte]: num_guest };
   }
   if (num_bed) {
-    roomQuery.num_guest = { [op.gte]: num_bed };
+    roomQuery.num_bed = { [op.gte]: num_bed };
   }
   if (num_bedroom) {
-    roomQuery.num_guest = { [op.gte]: num_bedroom };
+    roomQuery.num_bedroom = { [op.gte]: num_bedroom };
   }
   if (num_bathroom) {
-    roomQuery.num_guest = { [op.gte]: num_bathroom };
+    roomQuery.num_bathroom = { [op.gte]: num_bathroom };
   }
 
   req.roomQuery = roomQuery;
