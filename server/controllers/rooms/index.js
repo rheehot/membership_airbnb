1bed3ff34615a303167d6ba5580ba7d56467de48
aereeeee <dofl5576@gmail.com>
2019-10-17 22:47:57 +0900

    [FEATURE] 숙소 상세 조회에 캐싱 적용 (#4)
    why
    숙소 상세 조회 시  데이터를 캐싱하여 어플리케이션의 성능을 높인다
    what
    숙소 조회 시 redis 디비 먼저 검색하는 미들웨어 구현
    HIT : 데이터 응답한다.
    MISS : 디비쿼리 후 데이터 응답, 캐시 디비에 저장(만료시간은 1시간으로 설정)
    숙소 데이터 업데이트나 삭제 시 캐시 디비에서 데이터를 제거하는 미들웨어 구현

--- server/controllers/rooms/index.js
+++ server/controllers/rooms/index.js
@@ -1,11 +1,29 @@
 const { Router } = require('express');
-const { searchRoomCheck } = require('../../middlewares/searchRoomCheck');
+const { searchCache, deleteCache } = require('../../middlewares/roomCache');
+const roomSearch = require('../../middlewares/roomSearch');
+const auth = require('../../middlewares/auth');
+const csrfProtection = require('../../middlewares/csrf');
 
 const router = Router();
 const ctrl = require('./rooms.ctrl');
 
 router
   .get('/', ctrl.getAllRooms)
-  .get('/search', searchRoomCheck, ctrl.getFilteredRooms);
+  .get('/:id', searchCache, ctrl.getRoom)
+  .put(
+    '/:id',
+    csrfProtection,
+    auth.isAuthenticated,
+    deleteCache,
+    ctrl.updateRoom,
+  )
+  .delete(
+    '/:id',
+    csrfProtection,
+    auth.isAuthenticated,
+    deleteCache,
+    ctrl.deleteRoom,
+  )
+  .get('/search', roomSearch, ctrl.getFilteredRooms);
 
 module.exports = router;
--- server/controllers/rooms/rooms.ctrl.js
+++ server/controllers/rooms/rooms.ctrl.js
@@ -1,11 +1,49 @@
 const models = require('../../models');
 const paginate = require('../../utils/pagenation');
+const { saveCache } = require('../../middlewares/roomCache');
 
 const getAllRooms = async (req, res, next) => {
   const pageOption = paginate(req.query.page, process.env.PAGE_LIMIT);
   try {
     const rooms = await models.Room.findAll(pageOption);
     res.status(200).send(rooms);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getRoom = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const room = await models.Room.findOne({ where: { id } });
+    await saveCache(room);
+    res.status(200).send(room);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateRoom = async (req, res, next) => {
+  const { id } = req.params;
+  const { room } = req.body;
+
+  try {
+    const updatedRoom = await models.Room.update(room, {
+      where: { id },
+    });
+    res.status(200).send(updatedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteRoom = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await models.Room.destroy({
+      where: { id },
+    });
+    res.status(200).end();
   } catch (err) {
     next(err);
   }
@@ -31,4 +69,10 @@ const getFilteredRooms = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllRooms, getFilteredRooms };
+module.exports = {
+  getAllRooms,
+  getRoom,
+  updateRoom,
+  deleteRoom,
+  getFilteredRooms,
+};
