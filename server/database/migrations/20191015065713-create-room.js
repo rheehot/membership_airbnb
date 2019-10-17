3018ebfb5be8d2829e62ebf1c88d4bf4d47e4d87
aereeeee <dofl5576@gmail.com>
2019-10-17 13:42:30 +0900

    [FIX] 데이터베이스 기본키의 data type을 bigInt로 수정

--- server/database/migrations/20191015065713-create-room.js
+++ server/database/migrations/20191015065713-create-room.js
@@ -4,7 +4,7 @@ module.exports = {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
-      type: Sequelize.INTEGER,
+      type: Sequelize.BIGINT,
     },
     host_id: {
       type: Sequelize.INTEGER,
