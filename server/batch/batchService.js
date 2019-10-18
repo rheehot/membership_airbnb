1f8b49666e7ee9e28bf07e2786a45be8f9e45423
aereeeee <dofl5576@gmail.com>
2019-10-18 11:33:17 +0900

    [UTIL] batch job 폴더 분리 #5
    why
    하나의 task로 기능하기 때문에 utils폴더안에 위치하는것이 적합하지 않다고 판단

---
+++ server/batch/batchService.js
@@ -0,0 +1,43 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+const fs = require('fs');
+const Async = require('async');
+const csv = require('csv');
+const models = require('../models');
+
+const filePath = `${__dirname}/csv/room.csv`;
+
+const inserter = () => {
+  const parser = csv.parse({
+    columns: true,
+  });
+
+  const insert = Async.cargo(async (tasks) => {
+    try {
+      await models.Room.bulkCreate(tasks);
+      inserter.drain();
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  parser.on('readable', () => {
+    let line;
+    while ((line = parser.read())) {
+      insert.push(line);
+    }
+  });
+  parser.on('error', (err) => {
+    console.error('err', err.message);
+  });
+  parser.on('end', () => {
+    inserter.drain = () => {
+      console.log('완료');
+    };
+  });
+
+  fs.createReadStream(filePath).pipe(parser);
+};
+
+inserter();
