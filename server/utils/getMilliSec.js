e8f2a300fb53211fbb08709df381c31a0dcde5f3
aereeeee <dofl5576@gmail.com>
2019-10-18 11:33:54 +0900

    [CHORE] getMilliSec유틸 오류 수정

--- server/utils/getMilliSec.js
+++ server/utils/getMilliSec.js
@@ -1,6 +1,7 @@
-module.exports.milliseconds = (string) => {
+const milliseconds = (string) => {
   const regex = /([0-9])h([0-9])m([0-9])s/;
   const match = regex.exec(string);
   const timeVal = (match[1] * 60 * 60 + match[2] * 60 + match[3]) * 1000;
   return timeVal;
 };
+module.exports = milliseconds;
