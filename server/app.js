f2784b18b8e6abe797d253d06f0a99bb91a81d27
aereeeee <dofl5576@gmail.com>
2019-10-25 10:11:23 +0900

    [ CI ] client build 파일에 접근하기 위한 미들웨어 설정

--- server/app.js
+++ server/app.js
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', express.static(path.join(__dirname, 'build')));
 app.use('/api', require('./controllers'));
 
 app.use((req, res, next) => {
